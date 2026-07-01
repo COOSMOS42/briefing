52f41796fdca353dd8f725cd2022991ac898f560
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2026-07-01 10:39:32 -0300

    Update Briefing.py

--- Briefing.py
+++ Briefing.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pg = st.navigation([
-    st.Page("slit.py", title="Solicitar Peças"),
+    st.Page("solit.py", title="Solicitar Peças"),
     st.Page("consultas.py", title="Consultar Status")
 ])
 pg.run()
